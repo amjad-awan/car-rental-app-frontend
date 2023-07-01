b99634d4913fc583a6089fb9326137e89b723f45
Amjad Mehmood <amjad.mehmood@vigorant.com>
2023-07-01 14:29:47 +0500

    Initial commit from Create Next App

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCB9Mv6lHWgHP9sybZsFlIWfUEOG8LKqvg",
+  authDomain: "vehicle-rent-app.firebaseapp.com",
+  projectId: "vehicle-rent-app",
+  storageBucket: "vehicle-rent-app.appspot.com",
+  messagingSenderId: "300851390323",
+  appId: "1:300851390323:web:9b492d2b2e800b1ebcc706"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
